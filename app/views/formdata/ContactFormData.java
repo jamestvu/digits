e476f98841272dafab4b3c991aefb9d1da9726fa
jamestvu <jamesvu@hawaii.edu>
2013-10-14 16:13:31 -1000

    Attempt 2 of wod 3

--- app/views/formdata/ContactFormData.java
+++ app/views/formdata/ContactFormData.java
@@ -1,5 +1,9 @@
 package views.formdata;
 
+import java.util.ArrayList;
+import java.util.List;
+import play.data.validation.ValidationError;
+
 /**
  * Stores the Contact form data object.
  * 
@@ -8,6 +12,7 @@ package views.formdata;
  */
 public class ContactFormData {
   
+  private static final int NUM_TELEPHONE_DIGITS = 12;
   /** The first name. */
   public String firstName = "";
   /** The last name. */
@@ -15,4 +20,29 @@ public class ContactFormData {
   /** The phone number. */
   public String telephone = "";
   
+  /**
+   * Checks that form fields are valid. Called by bindFormRequest().
+   * @return null if valid, a list of ValidationError if problem is found.
+   */
+  public List<ValidationError> validate()  {
+    List<ValidationError> errors = new ArrayList<>();
+    
+    if (firstName == null || firstName.length() == 0) {
+      errors.add(new ValidationError("firstName", "First name is required."));
+    }
+    
+    if (lastName == null || lastName.length() == 0) {
+      errors.add(new ValidationError("lastName", "Last name is required."));
+    }
+    
+    if (telephone == null || telephone.length() == 0) {
+      errors.add(new ValidationError("telephone", "Telephone number is required."));
+    }
+    
+    if (telephone.length() != NUM_TELEPHONE_DIGITS) {
+      errors.add(new ValidationError("telephone", "Telephone number must follow the format xxx-xxx-xxxx."));
+    }
+    
+    return errors.isEmpty() ? null : errors;
+  }
 }

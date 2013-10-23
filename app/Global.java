4b85d8e9b28c5c761e7d338b94ef15e2f9716387
jamestvu <jamesvu@hawaii.edu>
2013-10-23 10:27:47 -1000

    Renamed select1 to select

--- app/Global.java
+++ app/Global.java
@@ -31,7 +31,7 @@ public class Global extends GlobalSettings {
       c1.firstName = "Phillip2";
       c1.lastName = "Johnson2";
       c1.telephone = "123-456-7890";
-      c1.telephoneType = "Home1";
+      c1.telephoneType = "Home";
       c1.standing = Standing.findLevel("Freshman").getName();
       ContactDB.addContact(c1);
       
@@ -39,15 +39,15 @@ public class Global extends GlobalSettings {
       c1.firstName = "Phillip3";
       c1.lastName = "Johnson3";
       c1.telephone = "123-456-7890";
-      c1.telephoneType = "Home1";
-      c1.standing = "burp";
+      c1.telephoneType = "Home";
+      c1.standing = "Senior";
       ContactDB.addContact(c1);
       
       c1 = new ContactFormData();
       c1.firstName = "Phillip4";
       c1.lastName = "Johnson4";
       c1.telephone = "123-456-7890";
-      c1.telephoneType = "Home1";
+      c1.telephoneType = "Home";
       c1.standing = Standing.findLevel("Junior").getName();
       ContactDB.addContact(c1);
       
